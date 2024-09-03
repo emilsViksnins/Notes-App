a9de3a6376fa5fe7702c812c5e60804e4da63565
Emils <135007928+emilsViksnins@users.noreply.github.com>
2024-09-03 09:59:41 +0300

    initial commit

---
+++ src/pages/NotesPage.jsx
@@ -0,0 +1,19 @@
+import NoteCard from "../components/NoteCard";
+import { useState, useEffect } from "react";
+import Controls from "../components/Controls";
+import { useContext } from "react";
+import { NotesContext } from "../context/NotesContext";
+
+const NotesPage = () => {
+    const { notes } = useContext(NotesContext);
+    return (
+        <div>
+            {notes.map((note) => (
+                <NoteCard note={note} key={note.$id} />
+            ))}
+            <Controls />
+        </div>
+    );
+};
+
+export default NotesPage;
